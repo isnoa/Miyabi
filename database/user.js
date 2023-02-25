5920d3d12e67ee7333f7103d1b187b4d544b54b3
NOA <isnoa3166@gmail.com>
2023-02-26 01:37:26 +0900

    다국어 데이터 제거

--- database/user.js
+++ database/user.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose')
 const Schema = new mongoose.Schema({
     since : String,
     user : String,
-    i18n : String,
     nowcharacter : String,
     profileconnect : Boolean,
     description : String,
