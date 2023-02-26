8e8d2cdec24459989307b685bf711397ef0e05b1
NOA <isnoa3166@gmail.com>
2023-02-27 01:04:59 +0900

    언어에 특정 구별 테그 설정

--- database/ko-kr.js
+++ database/ko-kr.js
@@ -1,22 +1,22 @@
 module.exports = {
     /** Languages */
-    Language_selection_Text: "언어를 선택해.",
-    Language_selection_Relax: "실수로 다른 언어를 선택하더라도 다시 선택할 수 있으니 걱정마.",
+    UILanguageSelect: "언어를 선택해.",
+    UILanguageRelax: "실수로 다른 언어를 선택하더라도 다시 선택할 수 있으니 걱정마.",
 
-    /** Profiles */
-    Profile_title_preview: "프로필 미리보기",
-    Profile_userFind_LookUp: "프로필 조회",
-    Profile_field_serviceRegistered: "서비스 가입",
-    Profile_field_recentlySearchedCharacter: "최근 검색한 캐릭터",
-    Profile_field_zzzConnect: "ZZZ 연동",
-    Profile_field_dailyCheckIn: "출석체크",
+    /** UIProfiles */
+    UIProfilePreview: "프로필 미리보기",
+    UIProfileLookUp: "프로필 조회",
+    UIProfileRegist: "서비스 가입",
+    UIProfileRSC: "최근 검색한 캐릭터",
+    UIProfileZZZConnect: "ZZZ 연동",
+    UIProfileDailyCheckIn: "출석체크",
 
-    /** Settings */
-    Setting_title_zzzConnect: "ZZZ 연동",
-    Setting_label_requiredValue: "필요한 값",
+    /** UISettings */
+    UISettingZZZConnect: "ZZZ 연동",
+    UISettingREQValue: "필요한 값",
 
-    /** Registers */
-    Register_description_retcodeZero: "등록 여부를 조사해봤는데 아무도 일치하지 않았어.",
+    /** UIRegisters */
+    UIRegisterRETCODEZero: "등록 여부를 조사해봤는데 아무도 일치하지 않았어.",
     
     /** Characters */
     anby_demara: "엔비 데마라",
@@ -32,46 +32,47 @@ module.exports = {
     soukaku: "소우카쿠",
     hoshimi_miyabi: "호시미 미야비",
 
-    Character_placeholder_text: "옵션을 선택해.",
+    UICharacterName: "이름",
+    UICharacterGender: "성별",
+    UICharacterBirthday: "생일",
+    UICharacter_Camp: "소속",
+    UICharacterElement: "속성",
+    UICharacterAttack: "공격",
 
-    Character_title_info: "정보",
-    Character_value_name: "이름",
-    Character_value_gender: "성별",
-    Character_label_birthday: "생일",
-    Character_value_camp: "소속",
-    Character_value_element: "속성",
-    Character_value_attack: "공격",
+    UICharacterNormalInfo: "기본정보",
+    UICharacterStats: "스텟",
+    UICharacterBasicAttack: "기본공격",
+    UICharacterSpecialAttack: "특수공격",
+    UICharacterComboAttack: "연계공격",
+    UICharacterDodge: "회피",
+    UICharacterTalent: "특성",
+    UICharacterPartyRecs: "추천파티",
 
-    Character_label_info: "정보",
-    Character_label_stats: "스텟",
-    Character_label_basicAttack: "기본공격",
-    Character_label_specialAttack: "특수공격",
-    Character_label_comboAttack: "연계공격",
-    Character_label_dodge: "회피",
-    Character_label_talent: "특성",
-    Character_label_partyRecs: "추천파티",
+    /** UIPlaceholder */
+    UIPlaceholderForCharacter: "옵션을 선택해.",
 
     /** Fight Props */
     FIGHT_PROP_HP: "HP",
-    FIGHT_PROP_ATTACK: "공격력",
-    FIGHT_PROP_DEFENSE: "방어력",
+    FIGHT_PROP_ATK: "공격력",
+    FIGHT_PROP_DEF: "방어력",
     FIGHT_PROP_HP_PERCENT: "HP%",
-    FIGHT_PROP_ATTACK_PERCENT: "공격력%",
-    FIGHT_PROP_DEFENSE_PERCENT: "방어력%",
+    FIGHT_PROP_ATK_PERCENT: "공격력%",
+    FIGHT_PROP_DEF_PERCENT: "방어력%",
+    FIGHT_PROP_IMPACT: "충격력",
     FIGHT_PROP_CRITICAL_RATE: "치명타 확률",
     FIGHT_PROP_CRITICAL_DMG: "치명타 피해",
-    FIGHT_PROP_PENETRATION_RATE: "관통률",
-    FIGHT_PROP_PENETRATION_FIGURE: "관통수치",
-    FIGHT_PROP_AUTOMATIC_ENERGY_RECOVERY: "에너지 자동 회복",
-    FIGHT_PROP_INFLICTING_EFFECT_DMG: "가하는 속성 피해",
-    FIGHT_PROP_AUTOMATIC_ENERGY_RECOVERY_RATE_PERCENT: "에너지 자동 회복 속도%",
+    FIGHT_PROP_PENETRATION_RATIO: "관통률",
+    FIGHT_PROP_PENETRATION: "관통수치",
+    FIGHT_PROP_ENERGY_RECOVERY: "에너지 자동 회복",
+    FIGHT_PROP_INFLICTING_DMG: "가하는 속성 피해",
+    FIGHT_PROP_ENERGY_RECOVERY_SPD_PERCENT: "에너지 자동 회복 속도%",
 
-    /** System */
+    /** UISystem */
     none: "없음",
     enabled: "사용중",
     disabled: "사용안함",
     true: "사용중",
     false: "사용안함",
     unknow: "알 수 없음",
-    failedToCheckData: " 관련된 데이터를 확인 할 수 없어.",
+    UIFTCD: " 관련된 데이터를 확인 할 수 없어.",
 }
