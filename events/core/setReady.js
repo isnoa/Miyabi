0f605d3fdcc4956f694704bb038d1dd0a77d61a5
NOA <isnoa3166@gmail.com>
2023-03-01 21:35:15 +0900

    서버 가입 문구 재설정

--- events/core/setReady.js
+++ events/core/setReady.js
@@ -23,15 +23,13 @@ client.on("guildCreate", async (guild) => {
           if (channel.permissionsFor(client.user).has("SEND_MESSAGES") === true) {
             const Embed = new EmbedBuilder()
               .setTitle('「질서를 어지럽히지 마」')
-              .setDescription('반가워 로프꾼. 나는 호시미 미야비야.\n내가 너를 위해 특별히 뉴에리두의 원칙과 질서를 잘 지킬 수 있도록 도와줄게. 그게 내 소임이기도 하고.\n\n내 도움을 받기 전에 먼저 </language:1028660728481194097>를 이용해서 너가 원하는 언어를 선택해 줘.\n언어를 설정하면 이용약관 및 개인정보 처리방침에 동의 하는 것으로 간주하니 참고하도록.')
+              .setDescription('반가워 로프꾼. 나는 호시미 미야비야.\n내가 너를 위해 특별히 뉴에리두의 원칙과 질서를 잘 지킬 수 있도록 도와줄게. 그게 내 소임이기도 하고.\n\n내 도움을 받을떄는 특정 명령어는 자동으로 이용약관 및 개인정보 처리방침에 동의 하는 것으로 간주하니 참고해 하지만 뭘 요구하거나 중요한건 아니니 안심해.')
               .setColor(MiyabiColor)
             if (channel.permissionsFor(client.user).has("EMBED_LINKS") === true) {
-              if (channel.permissionsFor(client.user).has("ATTACH_FILES") === true) {
-                Embed.setImage('https://cdn.discordapp.com/attachments/1019924590723612733/1065287053157347329/184908dc4ea4cacdc.jpg')
-              }
+                Embed.setImage('https://cdn.discordapp.com/attachments/1019924590723612733/1080400140080250880/184908dc4ea4cacdc.jpg')
             }
             channel.send({ embeds: [Embed] })
-            found = 1;
+            found = 2;
           }
         }
       }
