631ae61004606e0a224af734bf5b3ded35d60064
NOA <isnoa3166@gmail.com>
2023-03-11 03:07:00 +0900

    세팅 업데이트 딜레이 실시간 반영으로 개선

--- commands/context/settings.js
+++ commands/context/settings.js
@@ -6,6 +6,7 @@ const {
   StringSelectMenuBuilder
 } = require("discord.js");
 const { MiyabiColor } = require("../../database/color.js");
+const db = require("../../database/user.js");
 const text = require("../../database/ko-kr.js");
 
 module.exports = {
@@ -20,127 +21,132 @@ module.exports = {
   run: async (client, interaction) => {
     const userFind = interaction.user.id === interaction.targetId
     if (userFind) {
-      const Embed = new EmbedBuilder()
-        .setDescription(userData.description ?? "-\nㅤ")
-        .setFields(
-          {
-            name: text.Profile_field_serviceRegistered,
-            value: `<t:${parseInt(userData.since / 1000)}:R>`,
-            inline: true
-          },
-          {
-            name: text.Profile_field_recentlySearchedAgent,
-            value: text[userData.nowcharacter ?? "none"],
-            inline: true
-          },
-          {
-            name: text.Profile_field_zzzConnect,
-            value: text[!!userData.zzzconnect ?? "false"],
-            inline: true
-          },
-          {
-            name: text.Profile_field_dailyCheckIn,
-            value: text[userData.dailycheckin ?? "false"],
-            inline: true
-          }
-        )
-        .setThumbnail(interaction.user.avatarURL({ dynamic: true, size: 2048 }))
-        .setColor(MiyabiColor)
-
-      if (userData.profileconnect === true) {
-        Embed.setAuthor({ iconURL: "https://cdn.discordapp.com/emojis/1074016284292947969.png", name: text.Profile_title_preview + ` / ${text.Profile_userFind_LookUp}: ${text[userData.profileconnect]}` })
-      } else {
-        Embed.setAuthor({ iconURL: "https://cdn.discordapp.com/emojis/1074016285836451930.png", name: text.Profile_title_preview + ` / ${text.Profile_userFind_LookUp}: ${text[userData.profileconnect ?? "false"]}` })
-      }
-
-      if (userData.uid) {
-        Embed.setTitle(interaction.user.tag + `(${userData.uid})`)
-      } else {
-        Embed.setTitle(interaction.user.tag)
-      }
-
-      const ADDRow = new ActionRowBuilder().addComponents(
-        new StringSelectMenuBuilder()
-          .setCustomId("ADDSettingSelect")
-          .setPlaceholder(`추가 옵션을 선택해.`)
-          .setMaxValues(1)
-          .addOptions([
-            {
-              label: "프로필 조회 가능",
-              value: "ADDProfileConnect",
-              description: "프로필을 조회할 수 있게 할 수 있어."
-            },
-            {
-              label: "설명",
-              value: "ADDDescription",
-              description: "프로필 설명 추가할 수 있어."
-            },
-            {
-              label: "ZZZ 연동",
-              value: "ADDZZZConnect",
-              description: "게임 내에 정보를 볼 수 있어."
-            },
-            {
-              label: "출석체크",
-              value: "ADDdailyCheckIn",
-              description: "출석체크를 쓸 수 있어."
-            },
-          ])
-      )
-      const DELRow = new ActionRowBuilder().addComponents(
-        new StringSelectMenuBuilder()
-          .setCustomId("DELSettingSelect")
-          .setPlaceholder(`제거 옵션을 선택해.`)
-          .setMaxValues(1)
-          .addOptions([
-            {
-              label: "프로필 조회 불가",
-              value: "DELProfileConnect",
-              description: "프로필을 조회할 수 없게 할 수 있어."
-            },
-            {
-              label: "설명 제거",
-              value: "DELDescription",
-              description: "프로필 설명을 제거할 수 있어."
-            },
-            {
-              label: "ZZZ 연동 조회 불가",
-              value: "DELZZZConnect",
-              description: "ZZZ 연동 여부를 조회할 수 없게 할 수 있어."
-            },
-            {
-              label: "출석체크 조회 불가",
-              value: "DELDailyCheckIn",
-              description: "출석체크 여부를 조회할 수 없게 할 수 있어."
-            },
-          ])
-      )
-
-
-
-      if (["1010159742104113162", "893424082945720351"].includes(interaction.user.id)) {
-        const takumiRow = new ActionRowBuilder().addComponents(
-          new StringSelectMenuBuilder()
-            .setCustomId("TakumiSelect")
-            .setPlaceholder("-")
-            .setMaxValues(1)
-            .addOptions([
+      db.findOne({ user: interaction.user.id }, async (err, userData) => {
+        if (err) throw err;
+        if (userData) {
+          const Embed = new EmbedBuilder()
+            .setDescription(userData.description ?? "-\nㅤ")
+            .setFields(
               {
-                label: "뉴스 채널에 신규 코드 전송",
-                value: "codeSendToChannel",
-                description: "-"
+                name: text.UIProfileRegist,
+                value: `<t:${parseInt(userData.since / 1000)}:R>`,
+                inline: true
               },
               {
-                label: "-",
-                value: "-",
-                description: "-"
+                name: text.UIProfileRSC,
+                value: text[userData.nowcharacter ?? "none"],
+                inline: true
+              },
+              {
+                name: text.UIProfileZZZConnect,
+                value: text[!!userData.zzzconnect ?? "false"],
+                inline: true
+              },
+              {
+                name: text.UIProfileDailyCheckIn,
+                value: text[userData.dailycheckin ?? "false"],
+                inline: true
               }
-            ])
-        )
-        interaction.reply({ embeds: [Embed], components: [ADDRow, DELRow, takumiRow], ephemeral: true })
-      } else {
-        interaction.reply({ embeds: [Embed], components: [ADDRow, DELRow], ephemeral: true })
-      }
+            )
+            .setThumbnail(interaction.user.avatarURL({ dynamic: true, size: 2048 }))
+            .setColor(MiyabiColor)
+
+          if (userData.profileconnect === true) {
+            Embed.setAuthor({ iconURL: "https://cdn.discordapp.com/emojis/1074016284292947969.png", name: text.UIProfilePreview + ` / ${text.UIProfileLookUp}: ${text[userData.profileconnect]}` })
+          } else {
+            Embed.setAuthor({ iconURL: "https://cdn.discordapp.com/emojis/1074016285836451930.png", name: text.UIProfilePreview + ` / ${text.UIProfileLookUp}: ${text[userData.profileconnect ?? "false"]}` })
+          }
+
+          if (userData.uid) {
+            Embed.setTitle(interaction.user.tag + `(${userData.uid})`)
+          } else {
+            Embed.setTitle(interaction.user.tag)
+          }
+
+          const ADDRow = new ActionRowBuilder().addComponents(
+            new StringSelectMenuBuilder()
+              .setCustomId("ADDSettingSelect")
+              .setPlaceholder(`추가 옵션을 선택해.`)
+              .setMaxValues(1)
+              .addOptions([
+                {
+                  label: "프로필 조회 가능",
+                  value: "ADDProfileConnect",
+                  description: "프로필을 조회할 수 있게 할 수 있어."
+                },
+                {
+                  label: "설명",
+                  value: "ADDDescription",
+                  description: "프로필 설명 추가할 수 있어."
+                },
+                {
+                  label: "ZZZ 연동",
+                  value: "ADDZZZConnect",
+                  description: "게임 내에 정보를 볼 수 있어."
+                },
+                {
+                  label: "출석체크",
+                  value: "ADDdailyCheckIn",
+                  description: "출석체크를 쓸 수 있어."
+                },
+              ])
+          )
+          const DELRow = new ActionRowBuilder().addComponents(
+            new StringSelectMenuBuilder()
+              .setCustomId("DELSettingSelect")
+              .setPlaceholder(`제거 옵션을 선택해.`)
+              .setMaxValues(1)
+              .addOptions([
+                {
+                  label: "프로필 조회 불가",
+                  value: "DELProfileConnect",
+                  description: "프로필을 조회할 수 없게 할 수 있어."
+                },
+                {
+                  label: "설명 제거",
+                  value: "DELDescription",
+                  description: "프로필 설명을 제거할 수 있어."
+                },
+                {
+                  label: "ZZZ 연동 조회 불가",
+                  value: "DELZZZConnect",
+                  description: "ZZZ 연동 여부를 조회할 수 없게 할 수 있어."
+                },
+                {
+                  label: "출석체크 조회 불가",
+                  value: "DELDailyCheckIn",
+                  description: "출석체크 여부를 조회할 수 없게 할 수 있어."
+                },
+              ])
+          )
+
+
+
+          if (["1010159742104113162", "893424082945720351"].includes(interaction.user.id)) {
+            const takumiRow = new ActionRowBuilder().addComponents(
+              new StringSelectMenuBuilder()
+                .setCustomId("TakumiSelect")
+                .setPlaceholder("-")
+                .setMaxValues(1)
+                .addOptions([
+                  {
+                    label: "뉴스 채널에 신규 코드 전송",
+                    value: "codeSendToChannel",
+                    description: "-"
+                  },
+                  {
+                    label: "-",
+                    value: "-",
+                    description: "-"
+                  }
+                ])
+            )
+            interaction.reply({ embeds: [Embed], components: [ADDRow, DELRow, takumiRow], ephemeral: true })
+          } else {
+            interaction.reply({ embeds: [Embed], components: [ADDRow, DELRow], ephemeral: true })
+          }
+        }
+      })
     } else {
       interaction.reply({ content: "<:miyabi:1073113287102840892>", ephemeral: true })
     }
--- events/modules/SettingsFtn.js
+++ events/modules/SettingsFtn.js
@@ -9,6 +9,7 @@ const {
 } = require("discord.js");
 const db = require("../../database/user.js");
 const logger = require("../../events/core/logger.js");
+const { MiyabiColor } = require("../../database/color.js");
 const text = require("../../database/ko-kr.js");
 
 client.on("interactionCreate", async (interaction) => {
@@ -78,104 +79,119 @@ client.on("interactionCreate", async (interaction) => {
 
 
 
-    async function updateActRow() {
-        const text = require("../../database/ko-kr.js");
-        const color = interaction.member.displayHexColor;
-        const Embed = new EmbedBuilder()
-            .setDescription(userData.description ?? "-\nㅤ")
-            .setFields(
-                {
-                    name: text.Profile_field_serviceRegistered,
-                    value: `<t:${parseInt(userData.since / 1000)}:R>`,
-                    inline: true
-                },
-                {
-                    name: text.Profile_field_recentlySearchedAgent,
-                    value: text[userData.nowcharacter ?? "none"],
-                    inline: true
-                },
-                {
-                    name: text.Profile_field_zzzConnect,
-                    value: text[!!userData.zzzconnect ?? "false"],
-                    inline: true
-                },
-                {
-                    name: text.Profile_field_dailyCheckIn,
-                    value: text[userData.dailycheckin ?? "false"],
-                    inline: true
+    function updateActRow() {
+        db.findOne({ user: interaction.user.id }, async (err, userData) => {
+            if (err) throw err;
+            if (userData) {
+                const Embed = new EmbedBuilder()
+                    .setDescription(userData.description ?? "-\nㅤ")
+                    .setFields(
+                        {
+                            name: text.UIProfileRegist,
+                            value: `<t:${parseInt(userData.since / 1000)}:R>`,
+                            inline: true
+                        },
+                        {
+                            name: text.UIProfileRSC,
+                            value: text[userData.nowcharacter ?? "none"],
+                            inline: true
+                        },
+                        {
+                            name: text.UIProfileZZZConnect,
+                            value: text[!!userData.zzzconnect ?? "false"],
+                            inline: true
+                        },
+                        {
+                            name: text.UIProfileDailyCheckIn,
+                            value: text[userData.dailycheckin ?? "false"],
+                            inline: true
+                        }
+                    )
+                    .setThumbnail(interaction.user.avatarURL({ dynamic: true, size: 2048 }))
+                    .setColor(MiyabiColor)
+
+                if (userData.profileconnect === true) {
+                    Embed.setAuthor({ iconURL: "https://cdn.discordapp.com/emojis/1074016284292947969.png", name: text.UIProfilePreview + ` / ${text.UIProfileLookUp}: ${text[userData.profileconnect]}` })
+                } else {
+                    Embed.setAuthor({ iconURL: "https://cdn.discordapp.com/emojis/1074016285836451930.png", name: text.UIProfilePreview + ` / ${text.UIProfileLookUp}: ${text[userData.profileconnect ?? "false"]}` })
                 }
-            )
-            .setThumbnail(interaction.user.avatarURL({ dynamic: true, size: 2048 }))
-            .setColor(color ?? "#000000")
 
-        if (userData.profileconnect === true) {
-            Embed.setAuthor({ iconURL: "https://cdn.discordapp.com/emojis/1074016284292947969.png", name: text.Profile_title_preview + ` / ${text.Profile_userFind_LookUp}: ${text[userData.profileconnect]}` })
-        } else {
-            Embed.setAuthor({ iconURL: "https://cdn.discordapp.com/emojis/1074016285836451930.png", name: text.Profile_title_preview + ` / ${text.Profile_userFind_LookUp}: ${text[userData.profileconnect ?? "false"]}` })
-        }
+                if (userData.uid) {
+                    Embed.setTitle(interaction.user.tag + `(${userData.uid})`)
+                } else {
+                    Embed.setTitle(interaction.user.tag)
+                }
 
-        if (userData.uid) {
-            Embed.setTitle(interaction.user.tag + `(${userData.uid})`)
-        } else {
-            Embed.setTitle(interaction.user.tag)
-        }
-
-        const ADDRow = new ActionRowBuilder().addComponents(
-            new StringSelectMenuBuilder()
-                .setCustomId("ADDSettingSelect")
-                .setPlaceholder(`추가 옵션을 선택해.`)
-                .setMaxValues(1)
-                .ADDOptions([
-                    {
-                        label: "프로필 조회 가능",
-                        value: "ADDProfileConnect",
-                        description: "프로필을 조회할 수 있게 할 수 있어."
-                    },
-                    {
-                        label: "설명",
-                        value: "ADDDescription",
-                        description: "프로필 설명 추가할 수 있어."
-                    },
-                    // {
-                    //     label: "ZZZ 연동",
-                    //     value: "ADDZZZConnect",
-                    //     description: "ZZZ 연동으로 게임 내에 너의 정보를 볼 수 있어."
-                    // },
-                    {
-                        label: "출석체크",
-                        value: "ADDdailyCheckIn",
-                        description: "출석체크를 쓰는지 볼 수 있어."
-                    },
-                ])
-        )
-        const DELRow = new ActionRowBuilder().addComponents(
-            new StringSelectMenuBuilder()
-                .setCustomId("DELSettingSelect")
-                .setPlaceholder(`제거 옵션을 선택해.`)
-                .setMaxValues(1)
-                .ADDOptions([
-                    {
-                        label: "프로필 조회 불가",
-                        value: "DELProfileConnect",
-                        description: "프로필을 조회할 수 없게 할 수 있어."
-                    },
-                    {
-                        label: "설명 제거",
-                        value: "DELDescription",
-                        description: "프로필 설명을 제거할 수 있어."
-                    },
-                    // {
-                    //     label: "ZZZ 연동 조회 불가",
-                    //     value: "DELZZZConnect",
-                    //     description: "ZZZ 연동 여부를 조회할 수 없게 할 수 있어."
-                    // },
-                    {
-                        label: "출석체크 조회 불가",
-                        value: "DELDailyCheckIn",
-                        description: "출석체크 여부를 조회할 수 없게 할 수 있어."
-                    },
-                ])
-        )
-        await interaction.update({ embeds: [Embed], components: [ADDRow, DELRow], ephemeral: true })
+                const ADDRow = new ActionRowBuilder().addComponents(
+                    new StringSelectMenuBuilder()
+                        .setCustomId("ADDSettingSelect")
+                        .setPlaceholder(`추가 옵션을 선택해.`)
+                        .setMaxValues(1)
+                        .addOptions([
+                            {
+                                label: "프로필 조회 가능",
+                                value: "ADDProfileConnect",
+                                description: "프로필을 조회할 수 있게 할 수 있어."
+                            },
+                            {
+                                label: "설명",
+                                value: "ADDDescription",
+                                description: "프로필 설명 추가할 수 있어."
+                            },
+                            {
+                                label: "출석체크",
+                                value: "ADDdailyCheckIn",
+                                description: "출석체크를 쓰는지 볼 수 있어."
+                            },
+                        ])
+                )
+                const DELRow = new ActionRowBuilder().addComponents(
+                    new StringSelectMenuBuilder()
+                        .setCustomId("DELSettingSelect")
+                        .setPlaceholder(`제거 옵션을 선택해.`)
+                        .setMaxValues(1)
+                        .addOptions([
+                            {
+                                label: "프로필 조회 불가",
+                                value: "DELProfileConnect",
+                                description: "프로필을 조회할 수 없게 할 수 있어."
+                            },
+                            {
+                                label: "설명 제거",
+                                value: "DELDescription",
+                                description: "프로필 설명을 제거할 수 있어."
+                            },
+                            {
+                                label: "출석체크 조회 불가",
+                                value: "DELDailyCheckIn",
+                                description: "출석체크 여부를 조회할 수 없게 할 수 있어."
+                            },
+                        ])
+                )
+                if (["1010159742104113162", "893424082945720351"].includes(interaction.user.id)) {
+                    const takumiRow = new ActionRowBuilder().addComponents(
+                        new StringSelectMenuBuilder()
+                            .setCustomId("TakumiSelect")
+                            .setPlaceholder("-")
+                            .setMaxValues(1)
+                            .addOptions([
+                                {
+                                    label: "뉴스 채널에 신규 코드 전송",
+                                    value: "codeSendToChannel",
+                                    description: "-"
+                                },
+                                {
+                                    label: "-",
+                                    value: "-",
+                                    description: "-"
+                                }
+                            ])
+                    )
+                    interaction.update({ embeds: [Embed], components: [ADDRow, DELRow, takumiRow], ephemeral: true })
+                } else {
+                    interaction.update({ embeds: [Embed], components: [ADDRow, DELRow], ephemeral: true })
+                }
+            }
+        })
     }
 })
