1238b6e85b4761a7f1b4b34b31a1adc253a7e198
NOA <isnoa3166@gmail.com>
2023-03-11 12:00:50 +0900

    탈퇴 기능 구현

--- commands/cmd/isUnRegister.js
+++ commands/cmd/isUnRegister.js
@@ -7,7 +7,7 @@ const logger = require("../../events/core/logger.js");
 
 module.exports = {
     name: "탈퇴",
-    description: "ZZZ연동을 통한 기능을 사용하지 않도록 삭제.",
+    description: "가입을 탈퇴하는 하는걸 도와줄게.",
     timeout: 5000,
     /**
      *
@@ -19,10 +19,14 @@ module.exports = {
         db.findOne({ user: interaction.user.id }, async (err, userData) => {
             if (err) throw err;
             if (userData) {
+                const Embed = new EmbedBuilder()
+                .setTitle("탈퇴를 완료했어.")
+                .setDescription("탈퇴는 완료 했지만 쿠키(Cookie) & UID만 제거를 한 것일뿐 명령어를 처음 쓴 일자 및 최근에 검색해본 에이전트, 프로필 공개 여부, 로그 등은 지속될거야.")
+                interaction.reply({ embeds: [Embed] })
                 db.updateOne({ user: interaction.user.id }, { $set: { zzzconnect: "", uid: "" } })
                     .catch(err => logger.error(err));
             } else {
-                interaction.reply({ content: "그.. 가입도 안하고 탈퇴를 해달라면 어떻게 못해줘." })
+                interaction.reply({ content: "그.. 가입 이력이 없어서 탈퇴를 못 해줘." })
             }
         })
     }
