9c8406a1a1d090117b34a00f59eb6a2d8a2239f8
NOA <isnoa3166@gmail.com>
2023-03-20 21:50:48 +0900

    권고 사항 문구 추가

--- commands/cmd/isAgent.js
+++ commands/cmd/isAgent.js
@@ -32,91 +32,101 @@ module.exports = {
 	 */
 	run: async (client, interaction, args) => {
 		const name = interaction.options.getString("이름");
-			const matchedAgent = findOneAgent(name)
-			if (matchedAgent === undefined) return interaction.reply({ embeds: [new EmbedBuilder().setTitle("데이터매치 실패").setDescription(`\`\`\`${name}라는 이름을 찾을 수 없어.\`\`\`\n` + "다시 시도해보거나 개발자한테 물어보는게 좋을것 같아").setColor(MiyabiColor)] });
-			await axios.get(`https://zenlessdata.web.app/content_v2_user/app/3e9196a4b9274bd7/${matchedAgent}.json`).then(data => {
-				const Embed = new EmbedBuilder()
-					.setTitle(data.data.name + " - " + text.UIAgentInfo)
-					.setColor(data.data.colour)
-					.setDescription(`> ${(data.data.title).replace(/\n/i, "\n> ")}`)
-					.setFields(
+		const matchedAgent = findOneAgent(name)
+		if (matchedAgent === undefined) return interaction.reply({ embeds: [new EmbedBuilder().setTitle("데이터매치 실패").setDescription(`\`\`\`${name}라는 이름을 찾을 수 없어.\`\`\`\n` + "다시 시도해보거나 개발자한테 물어보는게 좋을것 같아").setColor(MiyabiColor)] });
+		await axios.get(`https://zenlessdata.web.app/content_v2_user/app/3e9196a4b9274bd7/${matchedAgent}.json`).then(data => {
+			function IFShouldReplace() {
+				if (matchedAgent === "soukaku") {
+					return `> ${(data.data.title).replace(/\n/i, " ")}`
+				} else if (matchedAgent === "ben_bigger") {
+					return `> ${(data.data.title).replace(/\n/i, " ")}`
+				} else {
+					return `> ${(data.data.title).replace(/\n/i, "\n> ")}`
+				}
+			}
+
+			const Embed = new EmbedBuilder()
+				.setTitle(data.data.name + " - " + text.UIAgentInfo)
+				.setColor(data.data.colour)
+				.setDescription(IFShouldReplace())
+				.setFields(
+					{
+						name: `기본 정보`,
+						value: `${text.UIAgentName}: ${data.data.name}\n${text.UIAgentGender}: ${data.data.gender}\n${text.UIAgentBirthDay}: ██월 ██일\n${text.UIAgentCamp}: ${data.data.camp}`,
+						inline: true
+					},
+					{
+						name: `전투 정보`,
+						value: `${text.UIAgentDamageAttribute}: 얼음\n${text.UIAgentAttackAttribute}: 베기\n→ *에테리얼류(상성)*`,
+						inline: true
+					},
+					{
+						name: `성우 정보`,
+						value: `일본어: ${data.data.cv.japanese}\n중국어: ${data.data.cv.chinese}`,
+						inline: true
+					},
+					{
+						name: "ㅤ",
+						value: `${data.data.interview}\n\n${data.data.intro}`,
+						inline: false
+					}
+				)
+				.setThumbnail(data.data.archive.avatar)
+				.setFooter({ text: text.UIPleaseKnowThat })
+			const row = new ActionRowBuilder().addComponents(
+				new StringSelectMenuBuilder()
+					.setCustomId("AgentSelect")
+					.setPlaceholder(text.UIPlaceholderForAgent)
+					.setMaxValues(1)
+					.addOptions([
 						{
-							name: `기본 정보`,
-							value: `${text.UIAgentName}: ${data.data.name}\n${text.UIAgentGender}: ${data.data.gender}\n${text.UIAgentBirthDay}: ██월 ██일\n${text.UIAgentCamp}: ${data.data.camp}`,
-							inline: true
+							label: text.UIAgentInfo,
+							value: "Info",
+							description: data.data.name + `의 ${text.UIAgentInfo} 알아보기`,
 						},
 						{
-							name: `전투 정보`,
-							value: `${text.UIAgentDamageAttribute}: 얼음\n${text.UIAgentAttackAttribute}: 베기\n→ *에테리얼류(상성)*`,
-							inline: true
+							label: text.UIAgentStats,
+							value: "Stats",
+							description: data.data.name + `의 ${text.UIAgentStats} 알아보기`,
 						},
 						{
-							name: `성우 정보`,
-							value: `일본어: ${data.data.cv.japanese}\n중국어: ${data.data.cv.chinese}`,
-							inline: true
+							label: text.UIAgentBasicAttack,
+							value: "BasicAttack",
+							description: data.data.name + `의 ${text.UIAgentBasicAttack} 알아보기`,
 						},
 						{
-							name: "ㅤ",
-							value: `${data.data.interview}\n\n${data.data.intro}`,
-							inline: false
+							label: text.UIAgentSpecialAttack,
+							value: "SpecialAttack",
+							description: data.data.name + `의 ${text.UIAgentSpecialAttack} 알아보기`,
+						},
+						{
+							label: text.UIAgentComboAttack,
+							value: "ComboAttack",
+							description: data.data.name + `의 ${text.UIAgentComboAttack} 알아보기`,
+						},
+						{
+							label: text.UIAgentDodge,
+							value: "Dodge",
+							description: data.data.name + `의 ${text.UIAgentDodge} 알아보기`,
+						},
+						{
+							label: text.UIAgentTalent,
+							value: "Talent",
+							description: data.data.name + `의 ${text.UIAgentTalent} 알아보기`,
+						},
+						{
+							label: text.UIAgentPartyRecs,
+							value: "PartyRecs",
+							description: data.data.name + `의 ${text.UIAgentPartyRecs} 알아보기`,
 						}
-					)
-					.setThumbnail(data.data.archive.avatar)
-				const row = new ActionRowBuilder().addComponents(
-					new StringSelectMenuBuilder()
-						.setCustomId("AgentSelect")
-						.setPlaceholder(text.UIPlaceholderForAgent)
-						.setMaxValues(1)
-						.addOptions([
-							{
-								label: text.UIAgentInfo,
-								value: "Info",
-								description: data.data.name + `의 ${text.UIAgentInfo} 알아보기`,
-							},
-							{
-								label: text.UIAgentStats,
-								value: "Stats",
-								description: data.data.name + `의 ${text.UIAgentStats} 알아보기`,
-							},
-							{
-								label: text.UIAgentBasicAttack,
-								value: "BasicAttack",
-								description: data.data.name + `의 ${text.UIAgentBasicAttack} 알아보기`,
-							},
-							{
-								label: text.UIAgentSpecialAttack,
-								value: "SpecialAttack",
-								description: data.data.name + `의 ${text.UIAgentSpecialAttack} 알아보기`,
-							},
-							{
-								label: text.UIAgentComboAttack,
-								value: "ComboAttack",
-								description: data.data.name + `의 ${text.UIAgentComboAttack} 알아보기`,
-							},
-							{
-								label: text.UIAgentDodge,
-								value: "Dodge",
-								description: data.data.name + `의 ${text.UIAgentDodge} 알아보기`,
-							},
-							{
-								label: text.UIAgentTalent,
-								value: "Talent",
-								description: data.data.name + `의 ${text.UIAgentTalent} 알아보기`,
-							},
-							{
-								label: text.UIAgentPartyRecs,
-								value: "PartyRecs",
-								description: data.data.name + `의 ${text.UIAgentPartyRecs} 알아보기`,
-							}
-						])
-				);
-				interaction.reply({ embeds: [Embed], components: [row] })
-				addHistory(matchedAgent)
-				logger.info(`File Director: (${__filename}) || User Id: [${interaction.user.id}] || Request Values: [${name}] || Interaction Latency: [${Math.abs(Date.now() - interaction.createdTimestamp)}ms] || API Latency: [${Math.round(client.ws.ping)}ms]`);
-			})
+					])
+			);
+			interaction.reply({ embeds: [Embed], components: [row] })
+			addHistory(matchedAgent)
+			logger.info(`File Director: (${__filename}) || User Id: [${interaction.user.id}] || Request Values: [${name}] || Interaction Latency: [${Math.abs(Date.now() - interaction.createdTimestamp)}ms] || API Latency: [${Math.round(client.ws.ping)}ms]`);
+		})
 
 		function addHistory(matchedAgent) {
-			// const userData = db.findOne({ user: interaction.user.id })
 			db.findOne({ user: interaction.user.id }).then((userData) => {
 				if (userData) {
 					db.updateOne({ user: interaction.user.id }, { $set: { lastcharacter: matchedAgent } })
