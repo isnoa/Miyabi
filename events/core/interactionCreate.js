38b91a402fdf66c39aa02e5920f1b228b421daba
NOA <isnoa3166@gmail.com>
2023-03-27 22:39:10 +0900

    문자 배열 수정

--- events/core/interactionCreate.js
+++ events/core/interactionCreate.js
@@ -4,7 +4,7 @@ client.on("interactionCreate", async (interaction) => {
   if (interaction.isChatInputCommand()) {
     const cmd = client.slashCommands.get(interaction.commandName);
     if (!cmd)
-      return interaction.reply({ content: "An error has occurred" });
+      return interaction.reply({ content: "오류가 발생했어." });
 
     const args = [];
 
@@ -19,15 +19,15 @@ client.on("interactionCreate", async (interaction) => {
     interaction.user = client.users.cache.get(interaction.user.id);
 
     if (cmd) {
-      if(cmd.timeout) {
-        if(client.timeout.has(`${cmd.name}${interaction.user.id}`))
-        return interaction.reply({ content: "진정해, 페이스 유지가 최선이야" })
+      if (cmd.timeout) {
+        if (client.timeout.has(`${cmd.name}${interaction.user.id}`))
+          return interaction.reply({ content: "진정해, 페이스 유지가 최선이야" })
         cmd.run(client, interaction, args);
         client.timeout.set(`${cmd.name}${interaction.user.id}`, Date.now() + cmd.timeout)
         setTimeout(() => {
-            client.timeout.delete(`${cmd.name}${interaction.user.id}`)
+          client.timeout.delete(`${cmd.name}${interaction.user.id}`)
         }, cmd.timeout)
-    }
+      }
     }
     // cmd.run(client, interaction, args);
   }
