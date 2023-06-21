9b4b5a3b6dff336d577dea7f0ad3b571a557f0fb
NOA <isnoa3166@gmail.com>
2023-06-21 17:58:22 +0900

    User-Agent에 Edg 추가

--- events/modules/dataMachine.js
+++ events/modules/dataMachine.js
@@ -5,7 +5,7 @@ const crypto = require('crypto');
 function createDataMachine(cookie) {
     const dataMachine = axios.create({
         headers: {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.46',
             Cookie: cookie,
             Accept: 'application/json;charset=utf-8',
             Referrer: 'https://webstatic-sea.mihoyo.com/',
