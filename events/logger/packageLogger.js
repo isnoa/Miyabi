53ee0a13de0fec40f7b49134da1254dd6fb3b468
NOA <isnoa3166@gmail.com>
2023-06-30 00:22:54 +0900

    packageLogger & 패키지 업데이트

---
+++ events/logger/packageLogger.js
@@ -0,0 +1,84 @@
+const { WebhookClient } = require('discord.js');
+
+const fetch = require('axios');
+const { version: axios } = require('axios/package.json');
+const { version: discordjs } = require('discord.js/package.json');
+const { version: dotenv } = require('dotenv/package.json');
+const { version: glob } = require('glob/package.json');
+const { version: mongoose } = require('mongoose/package.json');
+const { version: uuid } = require('uuid/package.json');
+
+const compareVersions = (currentVersion, latestVersion) => {
+  const current = currentVersion.split('.').map(Number);
+  const latest = latestVersion.split('.').map(Number);
+
+  for (let i = 0; i < current.length; i++) {
+    if (latest[i] > current[i]) {
+      return 'outdated';
+    } else if (latest[i] < current[i]) {
+      return 'up to date';
+    }
+  }
+
+  return 'up to date';
+};
+
+const checkPackageVersion = async (packageName, currentVersion) => {
+  try {
+    const { data } = await fetch.get(`https://registry.npmjs.org/${packageName}/latest`);
+    const latestVersion = data.version;
+    return {
+      packageName,
+      currentVersion,
+      latestVersion,
+      status: compareVersions(currentVersion, latestVersion)
+    };
+  } catch (error) {
+    console.error(`Failed to fetch the latest version of ${packageName}`);
+    return {
+      packageName,
+      currentVersion,
+      latestVersion: 'Failed to fetch',
+      status: 'failed'
+    };
+  }
+};
+
+const sendWebhookMessage = async (updates) => {
+  const webhook = new WebhookClient({ url: "https://canary.discord.com/api/webhooks/1123985218475393206/4yla0DXLp8UneLBkFLsk-wFUYPyr_sp3N-HbtlrRfey4fwQaWxWXW04y5Q7xvJ5k8iHQ" });
+
+  // 업데이트가 필요한 패키지만 필터링하여 메시지 생성
+  const message = updates
+    .map(update => `${update.packageName}: (now: ${update.currentVersion} / latest: ${update.latestVersion})`)
+    .join('\n');
+
+  const npmInstallCommands = updates
+    .map(update => `${update.packageName}`)
+    .join(' ');
+
+  // 웹훅을 통해 메시지 전송
+  await webhook.send("# 새로운 패키지 업데이트가 존재함\n\n" + message + "\n\n```bash\nnpm install " + npmInstallCommands + "\n```");
+};
+
+const runVersionChecks = async () => {
+  try {
+    const updates = await Promise.all([
+      checkPackageVersion('axios', axios),
+      checkPackageVersion('discord.js', discordjs),
+      checkPackageVersion('dotenv', dotenv),
+      checkPackageVersion('glob', glob),
+      checkPackageVersion('mongoose', mongoose),
+      checkPackageVersion('uuid', uuid)
+    ]);
+
+    // 업데이트가 필요한 패키지만 필터링하여 웹훅으로 보내기
+    const packagesToUpdate = updates.filter(update => update.status === 'outdated');
+    if (packagesToUpdate.length > 0) {
+      await sendWebhookMessage(packagesToUpdate);
+    }
+  } catch (error) {
+    console.error('An error occurred while checking package versions:', error);
+  }
+};
+
+runVersionChecks();
