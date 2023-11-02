01a110d52aeafc642cc8b0195cd321b50cf13f0a
NOA <isnoa3166@gmail.com>
2023-11-03 03:22:11 +0900

    초대 메세지 변경

--- src/events/handler/setReady.js
+++ src/events/handler/setReady.js
@@ -21,19 +21,17 @@ client.on("guildCreate", guild => {
   let found = 0;
   guild.channels.cache.map((channel) => {
     if (found === 0) {
-      if (channel.type === 0) {
-        if (channel.permissionsFor(client.user).has("0x0000000000000400")) {
-          if (channel.permissionsFor(client.user).has("0x0000000000000800")) {
-            const Embed = new EmbedBuilder()
-              .setTitle('「질서를 어지럽히지 마」')
-              .setDescription('반가워 로프꾼. 나는 미야비야.\n내가 너를 위해 특별히 뉴에리두의 원칙과 질서를 잘 지킬 수 있도록 도와줄게. 그게 내 소임이거든.\n\n내 도움을 받을떄는 특정 명령어는 자동으로 이용약관 및 개인정보 처리방침에 동의 하는 것으로 간주하니 참고해 하지만 뭘 요구하거나 중요한건 아니니 안심해.')
-              .setColor(text.MIYABI_COLOR)
-              .setImage('https://cdn.discordapp.com/attachments/1019924590723612733/1080400140080250880/184908dc4ea4cacdc.jpg')
-            channel.send({ embeds: [Embed] })
-            found = 1;
-          }
+      if (channel.isText()) {
+        if (channel.permissionsFor(client.user).has("SEND_MESSAGES") && channel.permissionsFor(client.user).has("EMBED_LINKS")) {
+          const Embed = new EmbedBuilder()
+            .setTitle('「질서를 어지럽히지 마」')
+            .setDescription('반가워 로프꾼.\n네가 뉴에리두에서 보다 편한 삶을 살 수 있도록 도울 거야.\n예를 들어.. 정원에 쌓인 눈을 쓸어 치우는 빗자루처럼, 응? 무슨 의미냐고? 글쎄. 별 것 아니야.\n\n내 도움을 받을 때는 몇 가지 인증 절차를 거쳐야 하는 경우가 있으니, 이용시 이용약관과 개인정보 처리방침을 꼭 확인해봐. 앞으로 잘 부탁해.')
+            .setColor(text.MIYABI_COLOR)
+            .setThumbnail('https://cdn.discordapp.com/attachments/1019924590723612733/1169700721193668608/Faction_Section_6_Icon.webp')
+          channel.send({ embeds: [Embed] })
+          found = 1;
         }
       }
     }
-  });
+  })
 });
